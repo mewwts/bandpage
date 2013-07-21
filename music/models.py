990ac65fbc8458e4d9234a4e51d5b8695cfbe757
leonjohannessen <leon@plysjbyen.net>
2013-07-21 23:53:39 +0200

    Added a new app, started writing some models and shit

---
+++ music/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length = 200)
+    lyrics = models.TextField(blank = True)
+    composer = models.CharField(max_length = 100)
+
+    is_single = models.BooleanField()
+    artwork_img = models.ImageField(upload_to='temp/folder', help_text='dimensions', blank=True) ## Fix later: Urls, only-if is_single
+
+    album = models.ManyToManyField(Album)
+
+    pub_date = models.DateTimeField('date published')
+    release_date = models.DateTimeField('date released')
+
+class Album(models.Model):
+    title = models.CharField(max_length = 200)
+
+    artwork_img = models.ImageField(upload_to='temp/folder', help_text='dimensions', blank=True) ## Fix later: Urls, only-if is_single
