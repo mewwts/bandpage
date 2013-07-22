497868df10625fbf1c2be7c7c6a3aa002f561ea5
mewwts <mats@plysjbyen.net>
2013-07-22 18:19:18 +0200

    woops

--- music/models.py
+++ music/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Song(models.Model):
-    title = models.CharField(max_length = 200)
-    lyrics = models.TextField(blank = True)
-    composer = models.CharField(max_length = 100)
+
+    title = models.CharField(max_length=200)
+    lyrics = models.TextField(blank=True)
+    composer = models.CharField(max_length=100)
 
     is_single = models.BooleanField()
     artwork_img = models.ImageField(upload_to='temp/folder', help_text='dimensions', blank=True) ## Fix later: Urls, only-if is_single
@@ -16,6 +17,7 @@ class Song(models.Model):
     release_date = models.DateTimeField('date released')
 
 class Album(models.Model):
-    title = models.CharField(max_length = 200)
+
+    title = models.CharField(max_length=200)
 
     artwork_img = models.ImageField(upload_to='temp/folder', help_text='dimensions', blank=True) ## Fix later: Urls, only-if is_single
