4647be4fac9c56a0c782193230b3883231501685
leonjohannessen <leon@plysjbyen.net>
2013-07-23 20:13:09 +0200

    Cleaned up music/models.py and validated some sql generation shizzle

--- music/models.py
+++ music/models.py
@@ -2,41 +2,6 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
-# Create your models here.
-
-class Song(models.Model):
-
-    title = models.CharField(max_length=200)
-    lyrics = models.TextField(blank=True)
-    composer = models.CharField(max_length=100)
-
-    is_single = models.BooleanField()
-    artwork_img = models.ImageField(upload_to='temp/folder', help_text='dimensions', blank=True) ## Fix later: Urls, only-if is_single
-
-    album = models.ManyToManyField(Album)
-
-    pub_date = models.DateTimeField('date published')
-    release_date = models.DateField('date released')
-
-    sales_link = generic.GenericRelation(SalesLink)
-
-
-class Album(models.Model):
-
-    title = models.CharField(max_length=200)
-
-    artwork_img = models.ImageField(upload_to='temp/folder', help_text='dimensions', blank=True) ## Fix later: Urls
-    booklet_file = models.FileField(upload_to='temp/folder', help_text='dimensions', blank=True) ## Fix later: Urls
-
-    pub_date = models.DateTimeField('date published')
-    release_date = models.DateField('date released')
-
-    is_vinyl = models.BooleanField()
-    vinyl_artwork_cover = models.ImageField(upload_to='temp/folder', help_text='dimensions', blank=True) ## Fix later: Urls, only if is_vinyl
-    vinyl_release_date = models.DateField('date released')
-    
-    sales_link = generic.GenericRelation(SalesLink)
-
 
 class SalesLink(models.Model):
 
@@ -53,10 +18,36 @@ class SalesLink(models.Model):
     text = models.CharField(max_length=100, blank=True)
     url = models.URLField()
 
-    content_type = models.ForignKey(ContentType)
+    content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey('content_type', 'object_id')
 
+
+class Album(models.Model):
+
+    title = models.CharField(max_length=200)
+    release_date = models.DateField('date released')
+    artwork_img = models.ImageField(upload_to='temp/folder', help_text='dimensions', blank=True) ## Fix later: Urls
+
+    is_vinyl = models.BooleanField()
+    vinyl_artwork_img = models.ImageField(upload_to='temp/folder', help_text='dimensions', blank=True) ## Fix later: Urls, only if is_vinyl
+    vinyl_release_date = models.DateField('date released')
+    
+    sales_link = generic.GenericRelation(SalesLink)
+    
+
+class Song(models.Model):
+
+    title = models.CharField(max_length=200)
+    release_date = models.DateField('date released')
+    composer = models.CharField(max_length=100)
+    lyrics = models.TextField(blank=True)
+
+    is_single = models.BooleanField()
+    single_artwork_img = models.ImageField(upload_to='temp/folder', help_text='dimensions', blank=True) ## Fix later: Urls, only-if is_single
+
+    album = models.ManyToManyField(Album)
+    sales_link = generic.GenericRelation(SalesLink)
 
 
 ##  TODO 
