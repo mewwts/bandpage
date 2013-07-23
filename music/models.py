8676d57b65fb693009cac88f0925d13b8ee9e1af
leonjohannessen <leon@plysjbyen.net>
2013-07-23 20:53:40 +0200

    Added human readable names for the music app models

--- music/models.py
+++ music/models.py
@@ -4,7 +4,6 @@ from django.contrib.contenttypes import generic
 
 
 class SalesLink(models.Model):
-
     LINK_TYPE_CHOICES = (
         ('cd', 'Compact Disc'),
         ('di', 'Digital'),
@@ -15,16 +14,19 @@ class SalesLink(models.Model):
     )
 
     type = models.CharField(choices=LINK_TYPE_CHOICES, max_length=2)
-    text = models.CharField(max_length=100, blank=True)
+    text = models.CharField(max_length=100, blank=True, help_text='Frontend alternate link text')
+    description = models.CharField(max_length=100, help_text='Description for administrative purposes')
     url = models.URLField()
 
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey('content_type', 'object_id')
 
+    def __unicode__(self):
+        return u'%s %s' % (self.type, self.description) 
+        
 
 class Album(models.Model):
-
     title = models.CharField(max_length=200)
     release_date = models.DateField('date released')
     artwork_img = models.ImageField(upload_to='temp/folder', help_text='dimensions', blank=True) ## Fix later: Urls
@@ -34,10 +36,12 @@ class Album(models.Model):
     vinyl_release_date = models.DateField('date released')
     
     sales_link = generic.GenericRelation(SalesLink)
+
+    def __unicode__(self):
+        return u'%s' % (self.title) 
     
 
 class Song(models.Model):
-
     title = models.CharField(max_length=200)
     release_date = models.DateField('date released')
     composer = models.CharField(max_length=100)
@@ -49,6 +53,9 @@ class Song(models.Model):
     album = models.ManyToManyField(Album)
     sales_link = generic.GenericRelation(SalesLink)
 
+    def __unicode__(self):
+        return u'%s' % (self.title) 
+        
 
 ##  TODO 
 ##  Media root in settings file 
