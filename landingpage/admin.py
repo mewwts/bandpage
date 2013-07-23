a633b37b0fc2a72cba656addf0851cd67aad9401
mewwts <mats@plysjbyen.net>
2013-07-23 22:10:04 +0200

    skriveleif

--- landingpage/admin.py
+++ landingpage/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from landingpage.models import Landingpage
 
-class LanndingAdmin(admin.ModelAdmin):
+
+class LandingAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['cover_image']}),
         ('Content', {'fields': ['title', 'link_url', 'text']}),
         ('Theme', {'fields': ['theme']}),
     ]
-admin.site.register(Landingpage, LanndingAdmin)
+admin.site.register(Landingpage, LandingAdmin)
