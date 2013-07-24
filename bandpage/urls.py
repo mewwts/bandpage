b5b3dabd5117b16ff0f81dc7b800573ee8be2fe6
mewwts <mats@plysjbyen.net>
2013-07-24 18:59:13 +0200

    minor fix

--- bandpage/urls.py
+++ bandpage/urls.py
@@ -14,6 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^landingpage/', include('landingpage.urls'))
+    url(r'^landingpage/', include('landingpage.urls')),
     url(r'^concerts/', include('concerts.urls'))
 )
