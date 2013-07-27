22e7fce73dc6e19d621014681ea133638d39eaa8
leonjohannessen <leon@plysjbyen.net>
2013-07-27 23:01:31 +0200

    Concert upcoming/past-events logic

--- concerts/views.py
+++ concerts/views.py
@@ -3,5 +3,9 @@ from concerts.models import Concert
 
 def index(request):
     concert_list = Concert.objects.all().order_by('date')
-    context = {'concert_list': concert_list}
+    upcoming_concerts = [concert for concert in concert_list if concert.is_upcoming()]
+    past_concerts = [concert for concert in concert_list if concert not in upcoming_concerts]
+    list = [upcoming_concerts, past_concerts]
+    context = {'concert_lists': list,
+               }
     return render(request, 'concerts/index.html', context)
