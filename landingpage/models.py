3d86bcba9ef279edd23cb17d90cb6f35dc454a7c
mewwts <mats@plysjbyen.net>
2013-07-27 16:06:21 +0200

    Added link text and blank=true on cover image.

--- landingpage/models.py
+++ landingpage/models.py
@@ -10,8 +10,9 @@ class Landingpage(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField(blank=True, null=True)
     link_url = models.URLField(max_length=200, blank=True, null=True)
+    link_text = models.CharField(max_length=200)
     cover_image = models.ImageField(upload_to='temp/folder',
-                                    help_text='dimensions')
+                                    help_text='dimensions', blank=True, null=True)
     theme = models.CharField(choices=THEME_CHOICES, max_length=4)
     homepage = UniqueBooleanField()
 
